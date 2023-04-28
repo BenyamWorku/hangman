459589465062a882441a4bc27b105c09f4f3594f
BenyamWorku <benworku@gmail.com>
2023-04-28 13:15:06 +0100

    checks users input

--- milestone_2.py
+++ milestone_2.py
@@ -2,4 +2,8 @@ import random
 word_list=["avocado","bananas","apples","mangoes","papaya"]
 word=random.choice(word_list)
 guess=input("Enter a single letter.")
+if guess.isalpha() and len(guess)==1 :
+    print("Good guess")
+else:
+    print("Oops,That isn't a valid input")
 print(word)
