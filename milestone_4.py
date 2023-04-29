0b65f155805cb182516cdc9dee526dc36aef0816
BenyamWorku <benworku@gmail.com>
2023-04-29 23:38:56 +0100

    changes space to underscore for word_guessed attribute

--- milestone_4.py
+++ milestone_4.py
@@ -3,7 +3,7 @@ class Hangman():
     def __init__(self,word_list,num_lives=5):
         self.word_list=word_list
         self.word=random.choice(self.word_list)
-        self.word_guessed=['']*len(self.word)
+        self.word_guessed=['_']*len(self.word)
         self.num_lives=num_lives
         self.num_letters=len(set(self.word))
         self.list_of_guesses=[]
