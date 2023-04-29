1b8d6b43ffd7517c5a0a25c8197f8d129b1e7e45
BenyamWorku <benworku@gmail.com>
2023-04-29 23:36:51 +0100

    creates class and initialised the attributes

---
+++ milestone_4.py
@@ -0,0 +1,11 @@
+import random
+class Hangman():
+    def __init__(self,word_list,num_lives=5):
+        self.word_list=word_list
+        self.word=random.choice(self.word_list)
+        self.word_guessed=['']*len(self.word)
+        self.num_lives=num_lives
+        self.num_letters=len(set(self.word))
+        self.list_of_guesses=[]
+        #list_of_guesses- A list of the guesses that have already been tried. Set this to an empty list initially.
+        pass
