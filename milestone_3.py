f8ef0df825883bd0e62071caa679288fc9f01ced
BenyamWorku <benworku@gmail.com>
2023-04-29 22:44:32 +0100

    modularises using 2 functions

--- milestone_3.py
+++ milestone_3.py
@@ -2,7 +2,7 @@ import random
 word_list=["avocado","bananas","apples","mangoes","papaya"]
 word=random.choice(word_list)
 print(word)
-while True:
+'''while True:
     guess=input("enter a letter of the alphabet.")
     if guess.isalpha() and len(guess)==1:
         if guess in word:
@@ -12,5 +12,26 @@ while True:
             print(f"sorry {guess} is not in the {word}.Try again.")
     else:
         print("Invalid letter. \
-              Please, enter a single alphabetical character.")
+              Please, enter a single alphabetical character.")'''
+
+def ask_for_input():
     
+    
+    while True:
+        guess=input("enter a letter of the alphabet.")
+        guess=guess.lower()
+        if guess.isalpha() and len(guess)==1:
+            break
+        else:
+            print("Invalid letter. \
+                Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+def check_guess(guess):
+  if guess in word:
+    print (f"yes {guess} is in the word {word}")
+  else :
+    print (f"sorry {guess} is not in the secret goword {word}.Try again.")
+
+# testing my function 
+ask_for_input()
